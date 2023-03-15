9c4036988fe1515a6d04e8561054a8f5f74bc569
Sharaf-Mansour <sharaf_1015@yahoo.com>
2023-03-15 20:15:32 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazeWind;
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+await builder.Build().RunAsync();
